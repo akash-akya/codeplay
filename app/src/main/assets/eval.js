31946d16c96dc4c5455e89d329530a583c49ef40
akash-akya <akashh246@gmail.com>
2018-06-09 21:42:26 +0530

    error message display

--- app/src/main/assets/eval.js
+++ app/src/main/assets/eval.js
@@ -1,15 +1,14 @@
-const eval_it = () => {
-        var show_error = function(e){
-            console.log("Error: "+e.message);
-            throw e;
-        };
-        var intp = new BiwaScheme.Interpreter(show_error);
-        try{
-            var ret = intp.evaluate(src);
-            Console.puts(";=> " + BiwaScheme.to_write(ret));
-        }
-        catch(e){
-            console.error(e);
-        }
-        dumper.dump_move(1);
+const eval_it = (str, errHandler, resultHandler) => {
+    var show_error = function(e){
+        console.log("Error: "+e.message);
+        throw e;
     };
+    var intp = new BiwaScheme.Interpreter(show_error);
+    try {
+        var ret = intp.evaluate(src);
+        Console.puts(";=> " + BiwaScheme.to_write(ret));
+    } catch(e){
+        console.error(e);
+    }
+    dumper.dump_move(1);
+};
