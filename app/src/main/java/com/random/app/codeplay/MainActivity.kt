3dcbbf5137355bc4aaed6ef0171337b07cd7bc79
Faraz <musaddiq.faraz1@gmail.com>
2018-06-09 12:26:49 +0530

    project create with empty activity

---
+++ app/src/main/java/com/random/app/codeplay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.random.app.codeplay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
